1e5d191c8fc17b4099322ace03e9ffaa974cf30d
Cameron Lucas <cameronlucasaim@gmail.com>
2021-03-21 23:08:46 -0600

    created global styled, imported font icon libs, import google fonts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import "normalize.css";
+import LandingPage from "./pages/landing";
+import GoogleFontLoader from "react-google-font-loader";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GlobalStyles = createGlobalStyle`
+@font-face {
+  font-family: "NTB";
+  src: url("/assets/fonts/NeutraText-Bold.otf") format("opentype");
 }
+
+* {
+  box-sizing: border-box;
+  font-family: 'Open Sans';
+  -webkit-font-smoothing: 'antialiased'
+}
+
+:root {
+  font-size: 16px;
+  line-height: 150%;
+
+  --JS-Yellow: 240, 219, 79;
+  --JS-Gray: 51, 51, 51;
+  --CL-Blue: 34, 40, 49;
+  --CL-Blue-Dark: 24, 28, 34;
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  background: rgb(34, 40, 49)
+}
+
+.wrapper {
+  width: 100%;
+  max-width: 64em;
+}
+`;
+
+const App = () => {
+  return (
+    <>
+      <GoogleFontLoader
+        fonts={[
+          {
+            font: "Open Sans",
+            weights: [
+              300,
+              "300i",
+              400,
+              "400i",
+              500,
+              600,
+              "600i",
+              700,
+              800,
+              900,
+              "900i",
+            ],
+          },
+        ]}
+      />
+      <GlobalStyles />
+      <Switch>
+        <Route path="/" exact component={LandingPage} />
+      </Switch>
+    </>
+  );
+};
 
 export default App;
