258901989bdb3c991d34b98cea54150cb34a893e
Cameron Lucas <cameronlucasaim@gmail.com>
2021-03-21 23:05:45 -0600

    created navbar component

---
+++ src/components/navbar/index.js
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+const Navigation = styled.nav`
+  width: 100%;
+  height: 3.125em;
+  background: rgb(var(--CL-Blue-Dark));
+  .wrapper {
+    margin: 0 auto;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    color: #fff;
+    .navbar-logo {
+      width: 3.125em;
+      height: auto;
+    }
+    .navbar-btn-list {
+      list-style: none;
+      display: flex;
+      flex-direction: row;
+      li {
+        margin: 0 0.625em;
+        height: 3.125em;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        align-items: center;
+        cursor: pointer;
+        i {
+          margin: 0 0.3125em;
+        }
+      }
+    }
+  }
+`;
+
+const Navbar = () => {
+  return (
+    <Navigation>
+      <div className="wrapper">
+        <img
+          src="/assets/images/logo.svg"
+          alt="cameron's logo"
+          className="navbar-logo"
+          draggable="false"
+        />
+        <ul className="navbar-btn-list">
+          <li>
+            <i className="fad fa-home" />
+            <span>Home</span>
+          </li>
+          <li>
+            <i className="fad fa-file-signature" />
+            <span>Projects</span>
+          </li>
+          <li>
+            <i className="fab fa-connectdevelop" />
+            <span>Let's Connect</span>
+          </li>
+        </ul>
+      </div>
+    </Navigation>
+  );
+};
+
+export default Navbar;
