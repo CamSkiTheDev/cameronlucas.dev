b0bd03a54121f4170082b1b44fe749aed7ec3741
Cameron Lucas <cameronlucasaim@gmail.com>
2021-05-16 17:06:28 -0600

    makes the button component a "Link"/"a" tag for seo reasons.

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-const StyledButton = styled.button`
+const StyledButton = styled(Link)`
   margin: 0.625em;
   padding: 0 1.25em;
   height: 1.875em;
@@ -12,6 +13,7 @@ const StyledButton = styled.button`
   background: ${({ buttonBGColor }) => (buttonBGColor ? buttonBGColor : null)};
   color: #fff;
   cursor: pointer;
+  text-decoration: none;
   i {
     margin: 0 0.625em 0 0;
   }
